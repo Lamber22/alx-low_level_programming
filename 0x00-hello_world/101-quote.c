a9294c42a30c4f3c5d94a09d6892c5cacf2c2c09
Lamber22 <Lamberjoe5@gmail.com>
2022-12-08 12:23:08 -0800

    Final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include  <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
