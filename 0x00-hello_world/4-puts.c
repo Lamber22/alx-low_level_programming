af554a1ea43d3cb3ce7cfbb72b5f2782ae71c901
Lamber22 <Lamberjoe5@gmail.com>
2022-12-08 08:09:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*using the puts function to print and the program is ending with the value of 0*/
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
