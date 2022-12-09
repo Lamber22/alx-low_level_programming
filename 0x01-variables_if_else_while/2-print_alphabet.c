31a7ee3884fe37478e5b7799e6080421c6199872
Lamber22 <Lamberjoe5@gmail.com>
2022-12-09 09:30:29 -0800

    betty problem fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - using putchar to print
- *
  * Return: always 0
  */
+
 int main(void)
 {
 	char xl;
+
 	for (xl = 'a'; xl <= 'z'; xl++)
 	{
 		putchar(xl);
