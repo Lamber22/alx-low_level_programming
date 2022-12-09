deb91208a8db33e5fb0a387c538c414f426855af
Lamber22 <Lamberjoe5@gmail.com>
2022-12-09 08:35:49 -0800

    hot fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
