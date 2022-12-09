f6604fda860bf1a52c49ce5613d4c9d5a0db1c10
Lamber22 <Lamberjoe5@gmail.com>
2022-12-09 11:33:29 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing base 10 numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int b10;
+		for (b10 = 10; b10 < 10; b10++)
+		putchar(b10);
+	putchar('\n');
+	return (0);
+}
