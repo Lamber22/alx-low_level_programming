52d58b3392d8968eb5b7e1a4cde1979ce3b3aa8d
Lamber22 <Lamberjoe5@gmail.com>
2022-12-13 08:26:27 -0800

    Wow

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z' (c++)) || (c >= 'A' && c <= 'Z' (c++)))
+	if (((c >= 'a' && c <= 'z') (c++)) || ((c >= 'A' && c <= 'Z') (c++)))
 		return (1);
 	else
 		return (0);
