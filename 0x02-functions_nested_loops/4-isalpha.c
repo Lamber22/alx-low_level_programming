6a82f3e5c5c0db2707f355b7d749da8d54a6c130
Lamber22 <Lamberjoe5@gmail.com>
2022-12-13 08:57:38 -0800

    Take 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,16 @@
 
 /**
  * _isalpha - checking for alphabetic character
- * @c: printing alpha characters
+ * @c: printing alphabetic characters
  * Return: 0 (Success)
  */
 
 int _isalpha(int c)
 {
-	if (((c >= 'a' && c <= 'z') (c++)) || ((c >= 'A' && c <= 'Z') (c++)))
+	for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
