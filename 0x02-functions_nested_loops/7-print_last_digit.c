df548be82f0f7b854eb9c1a3c53ea57b59a00cd0
Lamber22 <Lamberjoe5@gmail.com>
2022-12-13 23:31:05 -0800

    error fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of a number.
- * @n: digit to find the last place of.
+ * @nld: digit to find the last place of.
  * Return: The last digit.
  */
 
