9f9230fbf9491cdc8e772f93353921b6cfdd6220
Lamber22 <Lamberjoe5@gmail.com>
2022-12-14 17:05:59 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - printing the sum of all the multiles of 3 or 5
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
