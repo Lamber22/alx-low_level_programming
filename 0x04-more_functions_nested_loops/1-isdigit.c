8170d26f6d2e6bc4047b8004104998c9a9d40b80
Lamber22 <Lamberjoe5@gmail.com>
2022-12-15 11:24:10 -0800

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,7 +8,7 @@
 
 int _isdigit(int c)
 {
-	if (c >= 48; c <= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
@@ -16,5 +16,5 @@ int _isdigit(int c)
 	{
 		return (0);
 	}
-	return (0)
+	return (0);
 }
