97265b8f90989c03dc48fa51a9c89fd26fb53c3c
Lamber22 <Lamberjoe5@gmail.com>
2022-12-14 16:43:36 -0800

    new file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,42 +9,5 @@
 
 void print_times_table(int n)
 {
-	int r, c, res;
-
-	if (!(n > 1 || n < 0))
-	{
-		for (r = 0; r <= n; r++)
-		{
-			for (c = 0; c <= n; c++)
-			{
-				res = (r * c);
-				if (c != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && c != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && c != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+	return (0)
 }
