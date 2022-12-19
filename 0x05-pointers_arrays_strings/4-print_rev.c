aa809c0dcd46cad5526c1aac0ae451485c2208c9
Lamber22 <Lamberjoe5@gmail.com>
2022-12-19 11:32:07 -0800

    Tsk4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,10 @@ void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while (s[index++])
+	while (s[len] != '\0')
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index = len - 1 ; index >= 0 ; index--)
 		putchar(s[index]);
 
 	putchar('\n');
