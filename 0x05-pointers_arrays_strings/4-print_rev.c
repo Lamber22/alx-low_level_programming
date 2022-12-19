5e3e5beeaf7e40e7eade886fef63a8559ee97564
Lamber22 <Lamberjoe5@gmail.com>
2022-12-19 11:23:08 -0800

    file4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,16 @@
 /**
  * print_rev - a function that prints a string in reverse
  * @s: the string to print
- * Return: nothing
  */
 
 void print_rev(char *s)
 {
-	int lan = 0, index;
+	int len = 0, index;
 
 	while (s[index++])
-		lan++;
-	for (index = lan - 1; index >= 0; index--)
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
 		putchar(s[index]);
 
 	putchar('\n');
