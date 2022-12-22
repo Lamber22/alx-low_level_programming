de508dabc65b2f4e8cac37f13fb5bb976886bc13
Lamber22 <Lamberjoe5@gmail.com>
2022-12-22 09:24:36 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
-	
+
 	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
