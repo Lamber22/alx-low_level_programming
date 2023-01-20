9b469d1a7eac44ff2cebb73edd6d2091940f6256
Lamber22 <Lamberjoe5@gmail.com>
2023-01-20 04:12:08 -0800

    fixing errors in this code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
@@ -29,21 +30,21 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(ptr, int));
-			case 'i':
-				printf("%d", va_arg(ptr, int));
-			case 'f':
-				printf("%f", va_arg(ptr, double));
-			case 's':
-				str = va_arg(ptr, char *);
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
+		case 'c':
+			printf("%c", va_arg(ptr, int)), c = 1;
+		case 'i':
+			printf("%d", va_arg(ptr, int)), c = 1;
+		case 'f':
+			printf("%f", va_arg(ptr, double)), c = 1;
+		case 's':
+			str = va_arg(ptr, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
+			}
+			printf("%s", str);
+			break;
 		} i++;
 	}
 	va_end(ptr), printf("\n");
