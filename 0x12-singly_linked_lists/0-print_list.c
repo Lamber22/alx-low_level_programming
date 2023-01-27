a17447be07ab96082863b1b9eac3daeb373d35b7
Lamber22 <Lamberjoe5@gmail.com>
2023-01-26 16:43:38 -0800

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_list -  a function that prints all the elements of
+ * print_list -a function that prints all the elements of
  * a list_t list.
  * @h: The list_t list.
  *
