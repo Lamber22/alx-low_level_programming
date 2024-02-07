07d8b99866d15615c3596f167617fa7f48b13f58
Lamber22 <Lamberjoe5@gmail.com>
2024-02-06 19:38:27 -0800

    update

---
+++ 0x1E-search_algorithms/tests/102-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        0, 0, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, interpolation_search(array, size, 3));
